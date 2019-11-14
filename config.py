fde43b61845dcbe41d01cf7aa30418f2251288e6
hippoandy <ycaho@ucdavis.edu>
2019-11-13 20:33:48 -0800

    Add new variables

--- config.py
+++ config.py
@@ -3,12 +3,15 @@
 
 # database connection settings ------------------------------
 mdb_addr = '127.0.0.1'
-mdb_port = 3306
-mdb_user = 'andy'
+# mdb_port = 3306
+mdb_port = 8889
+mdb_user = 'reddit'
 mdb_pass = 'test'
+
+mdb_db = 'reddit_comments'
 # ------------------------------ database connection settings
 
-# file opertions --------------------------------------------
+# file/data/value opertions ---------------------------------
 read_buffer = 10000
 
 path_data = r'./data/'
@@ -16,6 +19,8 @@ path_failed = r'./error/'
 
 
 encoding_default = 'utf-8'
+
+tsstr_default = '%Y-%m-%d %H:%M:%S'
 # -------------------------------------------- file opertions
 
 # thread settings -------------------------------------------
